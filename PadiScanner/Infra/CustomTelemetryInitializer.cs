eb8486a25f0b0478dafb9d41f132128c61ed9e6f
fahminlb33 <fahminlb33@gmail.com>
2022-10-02 16:38:02 +0700

    Update cloud role

---
+++ PadiScanner/Infra/CustomTelemetryInitializer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace PadiScanner.Infra;
+
+public class CustomTelemetryInitializer : ITelemetryInitializer
+{
+    public void Initialize(ITelemetry telemetry)
+    {
+        if (string.IsNullOrEmpty(telemetry.Context.Cloud.RoleName))
+        {
+            //set custom role name here
+            telemetry.Context.Cloud.RoleName = "Frontend SPA";
+        }
+    }
+}
