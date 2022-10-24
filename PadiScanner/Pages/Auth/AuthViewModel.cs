2850b0d4df1ab94e0e7e9d2bdbb01441a0cd0261
fahminlb33 <fahminlb33@gmail.com>
2022-10-24 23:12:44 +0700

    Bugfix edit user and last login

--- PadiScanner/Pages/Auth/AuthViewModel.cs
+++ PadiScanner/Pages/Auth/AuthViewModel.cs
@@ -32,36 +32,47 @@ public class AuthViewModel : IAuthViewModel
 
     public async Task Login(string username, string password)
     {
+        // find the user
         var user = await _context.Users
             .AsNoTracking()
             .Where(x => x.Username == username)
             .FirstOrDefaultAsync();
 
+        // check if the user is found
         if (user == null)
         {
             throw new PadiException("Username atau password salah");
         }
 
+        // check if the password is correct
         if (!BCrypt.Net.BCrypt.Verify(password, user.Password))
         {
             throw new PadiException("Username atau password salah");
         }
 
+        // save last change
+        user.LastLoginAt = DateTime.Now;
+        await _context.SaveChangesAsync();
+
+        // set login state
         await _authProvider.LoginAsync(user);
     }
 
     public async Task GuestLogin()
     {
+        // find the guest login
         var user = await _context.Users
             .AsNoTracking()
             .Where(x => x.Role == UserRole.Guest)
             .SingleAsync();
 
+        // set login state
         await _authProvider.LoginAsync(user);
     }
 
     public async Task Logout()
     {
+        // remove login state
         await _authProvider.LogoutAsync();
     }
 }
